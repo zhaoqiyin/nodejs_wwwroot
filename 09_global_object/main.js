daa3cb9f7a3077df3352274184e09916fe22c61c
yinzhaoqi <yinzhaoqi@126.com>
2019-07-16 18:43:52 +0800

    练习node 路由和全局变量

---
+++ 09_global_object/main.js
@@ -0,0 +1,34 @@
+console.time("获取数据")
+// __filename 表示当前正在执行的脚本的文件名。它将输出文件所在位置的绝对路径，且和命令行参数所指定的文件名不一定相同。 如果在模块中，返回的值是模块文件的路径。
+// 输出全局变量 __filename 的值
+console.log( __filename );
+
+// __dirname 表示当前执行脚本所在的目录。
+// 输出全局变量 __dirname 的值
+console.log( __dirname );
+
+console.timeEnd("获取数据")
+
+// 输出到终端
+process.stdout.write("Hello World!" + "\n");
+
+// 通过参数读取
+process.argv.forEach(function(val, index, array) {
+   console.log(index + ': ' + val);
+});
+
+// 获取执行路径
+console.log(process.execPath);
+
+
+// 平台信息
+console.log(process.platform);
+
+// 输出当前目录
+console.log('当前目录: ' + process.cwd());
+
+// 输出当前版本
+console.log('当前版本: ' + process.version);
+
+// 输出内存使用情况
+console.log(process.memoryUsage());
